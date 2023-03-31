8e4ff81837b4bf7160fb87c8dfc563c65ba60475
Ankur G <16219421112@atmiyauni.edu.in>
2023-03-31 23:40:12 +0530

    Update 4_5.java

--- practical_4/4_5.java
+++ practical_4/4_5.java
@@ -1,16 +1,16 @@
-// Title: 4.5. Write a program to create a thread by implementing Runnable interface.
+// Title: 4.5. Write a program to create a thread by extending thread class.
 
-class Main implements Runnable {
-    public static void main(String[] args) {
-        Main m1 = new Main();
-        Thread t1 = new Thread(m1);
-        t1.start();
-    }
-
+public class MyThread extends Thread {
     public void run() {
         System.out.println("thread is running...");
     }
+
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
 }
+
 
 // output
 // thread is running...
